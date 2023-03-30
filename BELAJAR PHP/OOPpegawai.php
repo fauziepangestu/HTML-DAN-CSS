f8986955b43d38da102a87446b985afed6861369
fauziepangestu <126699563+fauziepangestu@users.noreply.github.com>
2023-03-31 05:36:22 +0700

    oop pegawai dan set grade

---
+++ BELAJAR PHP/OOPpegawai.php
@@ -0,0 +1,17 @@
+<?php
+require 'setgrades.php';
+
+$manager = new pegawai('01111', 'lutfy', 'Manager', 'Islam', 'Menikah');
+$asisten = new pegawai('01112', 'ipung', 'Asisten Manager', 'Islam', 'Belum Menikah');
+$keBag = new pegawai('01113', 'sandi', 'Kepala Bagian', 'Kong Hu Chu', 'Menikah');
+$staff = new pegawai('01114', 'rendy', 'Staff', 'Islam', 'Menikah');
+$staff2 = new pegawai('01115', 'rizky', 'Staff', 'Kristen', 'Menikah');
+
+$ar_pegawai = [$manager, $asisten, $keBag, $staff, $staff2];
+
+foreach ($ar_pegawai as $pegawai){
+    $pegawai-> cetak();
+}
+
+
+?>
