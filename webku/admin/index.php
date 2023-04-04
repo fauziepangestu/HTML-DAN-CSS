1f50dfa14dea9de8a68a87ab167dbf80a9e49de0
fauziepangestu <126699563+fauziepangestu@users.noreply.github.com>
2023-04-04 22:54:15 +0700

    web admin by uzi

---
+++ webku/admin/index.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+?>
+<div>
+    <class="container-fluid px-4">
+        <h1>Selamat datang di halaman admin</h1>
+        <?php
+    include_once 'registrasi.php';
+    ?>
+</div>
+<?php
+include_once 'bottom.php';
+?>
